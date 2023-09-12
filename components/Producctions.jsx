8de628af5c504dfe628ce162cb0fac11eccb3dee
MAKINWARA <luangarso@icloud.com>
2023-09-12 18:07:59 -0400

    FIx_Mobile_View

--- components/Producctions.jsx
+++ components/Producctions.jsx
@@ -3,6 +3,9 @@ import ViewMore from "./ViewMore";
 import "./Producctions.css";
 
 export default function Productions() {
+  const galleryText =
+    screen.width <= 1024 && screen.width > 390 ? "GALLERY" : "DANCE GALLERY";
+
   return (
     <div className="productions section_padding">
       <HeadLine
@@ -17,26 +20,28 @@ export default function Productions() {
         shadowColor={"rgba(255, 255, 255, 0.5)"}
       />
       {screen.width <= 375 ? (
-        <section className="gallery">
-          <div className="photos">
-            <figure className="beta one"></figure>
-          </div>
-          <article className="gallery_article">
-            <div>
-              <h3 className="gallery_h2">MONDRIAN DANCE</h3>
-              <p>
-                Le invitamos a estar al corriente de nuestras novedades y no
-                perderse ningún detalle de nuestras presentaciones, giras,
-                talleres, eventos, oportunidades, etc. Sea parte de nuestro
-                universo a cada instante.
-              </p>
-              <ViewMore text={"VIEW ALL PRODUCTIONS"} color={"Black"} />
+        <>
+          <section className="gallery">
+            <div className="photos">
+              <figure className="beta one"></figure>
             </div>
-          </article>
-        </section>
+            <article className="gallery_article">
+              <div>
+                <h3 className="gallery_h2">MONDRIAN DANCE</h3>
+                <p>
+                  Le invitamos a estar al corriente de nuestras novedades y no
+                  perderse ningún detalle de nuestras presentaciones, giras,
+                  talleres, eventos, oportunidades, etc. Sea parte de nuestro
+                  universo a cada instante.
+                </p>
+                <ViewMore text={"VIEW ALL PRODUCTIONS"} color={"Black"} />
+              </div>
+            </article>
+          </section>
+        </>
       ) : (
         <>
-          <strong>{screen.width <= 1024 ? "GALLERY" : "DANCE GALLERY"} </strong>
+          <strong>{galleryText}</strong>
           <section className="gallery">
             <div className="photos">
               <div className="photo_grid">
